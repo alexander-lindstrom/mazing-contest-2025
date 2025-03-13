3346d5b40e7b870d8a7f348971b5c108311de087
Alexander Lindström <alexander.b.lindstrom@gmail.com>
2025-03-13 21:05:44 +0100

    fix: grid render performance

--- client/src/components/GridRenderer.tsx
+++ client/src/components/GridRenderer.tsx
@@ -10,6 +10,7 @@ import { Grass } from "./grid/Grass";
 import { WildGrass } from "./grid/WildGrass";
 import { Sand } from "./grid/Sand";
 import { GridLines } from "./grid/GridLines";
+import React from "react";
 
 export interface BasePositionProps {
   x: number;
@@ -54,17 +55,14 @@ const GridRenderer: React.FC<GridRendererParams> = ({
 
   const handleMouseMove = useCallback(
     (e: KonvaEventObject<MouseEvent>) => {
-
+      if (grid.length === 0 || grid[0].length === 0) return;
       const stage = e.target.getStage();
       if (!stage) return;
-
       const pos = stage.getPointerPosition();
       if (!pos) return;
-
       const x = Math.floor(pos.x / CELL_SIZE);
       const y = Math.floor(pos.y / CELL_SIZE);
-
-      if (x >= 0 && x + 1 < grid[0].length && y >= 0 && y + 1 < grid.length) {
+      if (x >= 0 && x < grid[0].length && y >= 0 && y < grid.length) {
         setHoverPosition({ x, y });
       } else {
         setHoverPosition(null);
@@ -94,118 +92,153 @@ const GridRenderer: React.FC<GridRendererParams> = ({
     [handleClick, CELL_SIZE, grid]
   );
 
-  const renderBaseGrid = () => {
-    const gridElements = grid.map((row, y) =>
-      row.map((cell, x) => {
-        const centerX = (x + 0.5) * CELL_SIZE;
-        const centerY = (y + 0.5) * CELL_SIZE;
-        switch (cell) {
-          case GridCell.GRASS:
-            return (
-              <Grass
-                key={`grass-${x}-${y}`}
-                x={centerX}
-                y={centerY}
-                size={CELL_SIZE}
-              />
-            );
-          case GridCell.GRASS_NOBUILD:
-            return (
-              <WildGrass
-                key={`wild-grass-${x}-${y}`}
-                x={centerX}
-                y={centerY}
-                size={CELL_SIZE}
-              />
-            );
-          case GridCell.SAND:
-            return (
-              <Sand
-                key={`sand-${x}-${y}`}
-                x={centerX}
-                y={centerY}
-                size={CELL_SIZE}
-              />
-            );
-          default:
-            return null;
-        }
-      })
-    );
-  
-    return [
-      <GridLines key="grid-lines" width={grid[0].length * CELL_SIZE} height={grid.length * CELL_SIZE} cellSize={CELL_SIZE} />,
-      ...gridElements.flat(),
-    ];
-  };
-  
+  interface RenderBaseGridProps {
+    grid: GridCell[][];
+    CELL_SIZE: number;
+    towers: Tower[];
+  }
 
-  const renderTowers = () => {
-
-    return towers.map((tower, index) => {
-      const { positions } = tower;
-      const center = getCenterPoint(positions);
-      
-      const centerX = (center.x + 0.5) * CELL_SIZE;
-      const centerY = (center.y + 0.5) * CELL_SIZE;
-      
-      const radius = 2*CELL_SIZE;
-      
-      switch (tower.type) {
-        case GridCell.CLAP_TOWER:
-          return (
-            <SlowTower
-              key={`tower-${index}`}
-              id={`tower-${index}`}
-              x={centerX}
-              y={centerY}
-              towerRadius={radius}
-              showEffectBorder={false} // Todo: show on hover?
-              sellable={true}       
-            />
-          );
-        case GridCell.CLAP_TOWER_NOSELL:
-          return (
-            <SlowTower
-              key={`tower-${index}`}
-              id={`tower-${index}`}
-              x={centerX}
-              y={centerY}
-              towerRadius={radius}
-              showEffectBorder={false} // Todo: show on hover?
-              sellable={false}       
-            />
-          );
-        case GridCell.BLOCK_TOWER:
+  const renderBaseGrid = ({ grid, CELL_SIZE, towers }: RenderBaseGridProps) => {
+    return useMemo(() => {
+      const gridElements = grid.map((row, y) =>
+        row.map((cell, x) => {
+          const centerX = (x + 0.5) * CELL_SIZE;
+          const centerY = (y + 0.5) * CELL_SIZE;
+          switch (cell) {
+            case GridCell.GRASS:
+              return (
+                <Grass
+                  key={`grass-${x}-${y}`}
+                  x={centerX}
+                  y={centerY}
+                  size={CELL_SIZE}
+                />
+              );
+            case GridCell.GRASS_NOBUILD:
+              return (
+                <WildGrass
+                  key={`wild-grass-${x}-${y}`}
+                  x={centerX}
+                  y={centerY}
+                  size={CELL_SIZE}
+                />
+              );
+            case GridCell.SAND:
+              return (
+                <Sand
+                  key={`sand-${x}-${y}`}
+                  x={centerX}
+                  y={centerY}
+                  size={CELL_SIZE}
+                />
+              );
+            default:
+              return null;
+          }
+        })
+      );
+  
+      const unsellableTowers = towers
+        .filter((tower) => tower.type === GridCell.BLOCK_TOWER_NOSELL)
+        .map((tower, index) => {
+          const { positions } = tower;
+          const center = getCenterPoint(positions);
+          const centerX = (center.x + 0.5) * CELL_SIZE;
+          const centerY = (center.y + 0.5) * CELL_SIZE;
+          const radius = 2 * CELL_SIZE;
+  
           return (
             <BlockingTower
-              key={`tower-${index}`}
-              id={`tower-${index}`}
-              x={centerX}
-              y={centerY}
-              towerRadius={radius}
-              sellable={true}
-            />
-          );
-        case GridCell.BLOCK_TOWER_NOSELL:
-          return (
-            <BlockingTower
-              key={`tower-${index}`}
-              id={`tower-${index}`}
+              key={`unsellable-tower-${index}`}
               x={centerX}
               y={centerY}
               towerRadius={radius}
               sellable={false}
             />
           );
-        default:
-          return null;
-      }
-    });
+        });
+  
+      return (
+        <Layer cache>
+          {gridElements.flat()}
+          {unsellableTowers}
+          <GridLines
+            key="grid-lines"
+            width={grid[0].length * CELL_SIZE}
+            height={grid.length * CELL_SIZE}
+            cellSize={CELL_SIZE}
+          />
+        </Layer>
+      );
+    }, [grid, CELL_SIZE, towers]);
   };
+  
+  const towersGrid = useMemo(() => 
+    towers.map((tower, index) => {
+      const { positions, type } = tower;
+      const center = getCenterPoint(positions);
+      
+      const centerX = (center.x + 0.5) * CELL_SIZE;
+      const centerY = (center.y + 0.5) * CELL_SIZE;
+      const radius = 2 * CELL_SIZE;
+      const key = `tower-${index}`;
+  
+      if (type === GridCell.CLAP_TOWER || type === GridCell.CLAP_TOWER_NOSELL) {
+        return (
+          <SlowTower
+            key={key}
+            id={key}
+            x={centerX}
+            y={centerY}
+            towerRadius={radius}
+            showEffectBorder={false} // Todo: show on hover?
+            sellable={type === GridCell.CLAP_TOWER}       
+          />
+        );
+      }
+  
+      if (type === GridCell.BLOCK_TOWER) {
+        return (
+          <BlockingTower
+            key={key}
+            id={key}
+            x={centerX}
+            y={centerY}
+            towerRadius={radius}
+            sellable={true}
+          />
+        );
+      }
+  
+      return null;
+    }),
+    [towers, CELL_SIZE]);
 
-  const baseGrid = useMemo(() => renderBaseGrid(), [grid, CELL_SIZE]);
-  const towersGrid = useMemo(() => renderTowers(), [towers, CELL_SIZE]);
+    const hoverEffect = useMemo(() => {
+      if (!hoverPosition || showRunner) return null;
+      return (
+        <Group>
+          {[0, 1].map((dy) =>
+            [0, 1].map((dx) => {
+              const x = hoverPosition.x + dx;
+              const y = hoverPosition.y + dy;
+              return (
+                <Rect
+                  key={`hover-${x}-${y}`}
+                  x={x * CELL_SIZE}
+                  y={y * CELL_SIZE}
+                  width={CELL_SIZE}
+                  height={CELL_SIZE}
+                  fill="rgba(255, 255, 255, 0.2)"
+                  stroke={canPlaceTower(grid, hoverPosition.x, hoverPosition.y) ? "blue" : "red"}
+                  strokeWidth={2}
+                />
+              );
+            })
+          )}
+        </Group>
+      );
+    }, [hoverPosition, showRunner, CELL_SIZE, grid]);
 
   return (
     <Stage
@@ -215,34 +248,13 @@ const GridRenderer: React.FC<GridRendererParams> = ({
       onMouseLeave={handleMouseLeave}
       onClick={handleStageClick}
     >
-      <Layer>{baseGrid}
-      <GridLines width={width} height={height} cellSize={CELL_SIZE} />
+      {renderBaseGrid({ grid, CELL_SIZE, towers })}
+      <Layer>
+        {towersGrid}
       </Layer>
-      <Layer>{towersGrid}</Layer>
-      
+
       {!showRunner && hoverPosition && (
-        <Layer>
-          <Group>
-            {[0, 1].map((dy) =>
-              [0, 1].map((dx) => {
-                const x = hoverPosition.x + dx;
-                const y = hoverPosition.y + dy;
-                return (
-                  <Rect
-                    key={`hover-${x}-${y}`}
-                    x={x * CELL_SIZE}
-                    y={y * CELL_SIZE}
-                    width={CELL_SIZE}
-                    height={CELL_SIZE}
-                    fill="rgba(255, 255, 255, 0.2)"
-                    stroke={canPlaceTower(grid, hoverPosition.x, hoverPosition.y) ? "blue" : "red"}
-                    strokeWidth={2}
-                  />
-                );
-              })
-            )}
-          </Group>
-        </Layer>
+        <Layer>{hoverEffect}</Layer>
       )}
       {runnerPath && showRunner && (
         <Layer>
@@ -264,4 +276,4 @@ const GridRenderer: React.FC<GridRendererParams> = ({
   );
 };
 
-export default GridRenderer;
+export default React.memo(GridRenderer);
